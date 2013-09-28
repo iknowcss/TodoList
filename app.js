b8d1ca9ea6197ac38b07178deb2f2130ecaf72e3
iknowcss <iknowcss@gmail.com>
2013-09-28 16:11:46 -0700

    Add more configurations
    - Add JsBeautify for autoformatting code
    - Add 12-column 960 grid
    - Create base styles
    - Write index.js with description of project

---
+++ app.js
@@ -0,0 +1,38 @@
+var express = require('express'),
+  http = require('http'),
+  path = require('path'),
+
+  listenPort = process.env.PORT || 3000,
+  viewsPath = path.join(__dirname, 'views'),
+  publicPath = path.join(__dirname, 'public'),
+
+  app = express(),
+  routes = require('./routes');
+
+// Setup
+app.set('port', listenPort);
+app.set('views', viewsPath);
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(app.router);
+app.use(require('less-middleware')({
+  src: publicPath
+}));
+app.use(express.static(publicPath));
+
+// Routes
+app.get('/', routes.index);
+
+// Development Settings
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+http.createServer(app).listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
